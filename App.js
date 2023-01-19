039419512a185ee1fe7434826618baa378202f85
ultronpatil <120434518+ultronpatil@users.noreply.github.com>
2023-01-19 07:57:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,383 @@
+import { Diamond } from "react-awesome-shapes";
+import { PolygonCard } from "react-awesome-shapes/dist/shapes/polygonCard";
+import React from "react";
+import './App.css';
+
+function App() {
+
+  return (
+    
+   <div className="App">
+      <header>
+        <h1 className="head">(DAIPL) Product Developement Process
+        </h1>   
+      </header>
+        <h4 className="m1">kick-off</h4>
+        <h4 className="m2">DRK</h4>
+        <h4 className="m3">DRII</h4>
+        <h4 className="m4">MT</h4>
+        <h4 className="m5">DRI</h4>
+        <h4 className="m6">FMT</h4>
+        <h4 className="m7">DRF</h4>
+        <h4 className="m8">PJ</h4>
+        <h4 className="m9">PT</h4>
+        <h4 className="m10">FJ</h4>
+        <h4 className="m11">MP</h4>
+
+        <h4 className="n1">QDRII</h4>
+        <h4 className="n2">CDRII</h4>
+        <h4 className="n3">PDRII</h4>
+
+        <h4 className="n4">QDRI</h4>
+        <h4 className="n5">CDRI</h4>
+        <h4 className="n6">PDRI</h4>
+
+        <h4 className="n7">QDRF</h4>
+        <h4 className="n8">CDRF</h4>
+        <h4 className="n9">PDRF</h4>
+
+      <Diamond
+        color="blue"
+        left="100px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+
+    <PolygonCard
+      top="115px"
+      left="130px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+
+      <Diamond
+        color="blue"
+        left="155px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="190px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="210px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="240px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="265px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="300px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="320px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="350px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="375px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="410px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="430px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="460px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="485px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="515px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="540px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="575px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="595px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    <PolygonCard
+      top="115px"
+      left="630px"
+      height="10px"
+      width="50px"
+      zIndex={-2}
+      color="maroon"
+    />
+      <Diamond
+        color="blue"
+        left="650px"
+        position="absolute"
+        top="100px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+
+
+      
+      <PolygonCard
+      top="157px"
+      left="224.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />      
+
+        <Diamond
+        color="grey"
+        left="210px"
+        position="absolute"
+        top="175px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+      <PolygonCard
+      top="210px"
+      left="224.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />
+        <Diamond
+        color="grey"
+        left="210px"
+        position="absolute"
+        top="235px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+            <PolygonCard
+      top="270px"
+      left="224.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />
+              <Diamond
+        color="grey"
+        left="210px"
+        position="absolute"
+        top="295px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+       <PolygonCard
+      top="157px"
+      left="334.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />      
+        <Diamond
+        color="grey"
+        left="320px"
+        position="absolute"
+        top="175px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+      <PolygonCard
+      top="210px"
+      left="334.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />  
+        <Diamond
+        color="grey"
+        left="320px"
+        position="absolute"
+        top="235px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+      <PolygonCard
+      top="270px"
+      left="334.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      /> 
+
+      <PolygonCard
+      top="157px"
+      left="444.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      /> 
+        <Diamond
+        color="grey"
+        left="320px"
+        position="absolute"
+        top="295px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+            <PolygonCard
+      top="210px"
+      left="444.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />
+        <Diamond
+        color="grey"
+        left="430px"
+        position="absolute"
+        top="175px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+            <PolygonCard
+      top="270px"
+      left="444.5px"
+      height="50px"
+      width="10px"
+      zIndex={-2}
+      color="orange"
+      />
+        <Diamond
+        color="grey"
+        left="430px"
+        position="absolute"
+        top="235px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+        <Diamond
+        color="grey"
+        left="430px"
+        position="absolute"
+        top="295px"
+        size="70px"
+        //size={['100px','100px','200px','200px']}
+        zIndex={1}
+      />
+    </div>
+    
+  );
+}
+
+export default App;
